360e19064d14c11105b9056fed458aa81c243f88
Henrique Dantas <henrique@hotmail.ph>
2022-01-30 09:25:26 -0300

    chakraui template with nextjs

---
+++ styles/theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  fonts: {
+    heading: "Poppins",
+    body: "Poppins",
+  },
+  styles: {
+    body: {
+      color: "#0000",
+    },
+  },
+});
